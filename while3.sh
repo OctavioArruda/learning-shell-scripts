fc035845ef887d4b2f5fc8845b2ba1c8e9b9cdae
Octavio Arruda <oneiros241@gmail.com>
2020-12-06 10:07:00 -0300

    :sparkles: loops: while

---
+++ while3.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# What this scripts does is read from the file named myfile and for each line of the
+# myfile file it prints the correct language based on the lines entries
+while read f 
+do
+    case $f in
+        hello)       echo English     ;;
+        howdy)       echo American    ;;
+        gday)        echo Australian  ;;
+        bonjour)     echo French      ;;
+        "guten tag") echo German      ;;
+        "bom dia")   echo Brazilian   ;;
+        *)           echo Unknown language: $f ;;
+    esac 
+done < myfile
+         
